69ddac6b98b1858420e2154f8d7068da453b414d
bhenjayamparo-ctrl <bhenjayamparo@gmail.com>
2026-09-21 18:57:59 +0800

    Clean up middleware.php by removing comments
    Removed license and author information from middleware.php.

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -1,46 +1,6 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
-/**
- * ------------------------------------------------------------------
- * LavaLust - an opensource lightweight PHP MVC Framework
- * ------------------------------------------------------------------
- *
- * MIT License
- *
- * Copyright (c) 2020 Ronald M. Marasigan
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package LavaLust
- * @author Ronald M. Marasigan <ronald.marasigan@yahoo.com>
- * @since Version 4
- * @link https://github.com/ronmarasigan/LavaLust
- * @license https://opensource.org/licenses/MIT MIT License
- */
-/*
-|--------------------------------------------------------------------------
-| Adding of middlewares
-|--------------------------------------------------------------------------
-|
-| Used for adding middlewares
-|
-*/
+
 require_once APP_DIR . 'middlewares/AuthMiddleware.php';
 require_once APP_DIR . 'middlewares/GuestMiddleware.php';
 
@@ -48,10 +8,4 @@ $middleware_config = [
     'auth'  => new AuthMiddleware(),
     'guest' => new GuestMiddleware(),
 ];
-
-// Make the middleware map available to the Middleware kernel class
-// right away. This file is required directly from routes.php, which
-// runs before the router dispatches -- earlier than the normal config
-// autoload (which only fires once a controller is constructed, too
-// late for middleware to guard a route).
 get_config(['middlewares' => $middleware_config]);
